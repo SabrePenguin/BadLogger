585bc58f2cfdae2b79d94ace1247ea0e40cead8e
SabrePenguin <lknofczynski@gmail.com>
2026-04-25 21:35:37 -0400

    Move files around

---
+++ src/main/java/com/sabrepenguin/crashlogger/helper/LogFileManager.java
@@ -0,0 +1,56 @@
+package com.sabrepenguin.crashlogger.helper;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class LogFileManager {
+	private static final MappedByteBuffer crashBuffer;
+	private static final int MAX_FUNCS = 1;
+	public static final String[] MESSAGES = {
+			"GlStateManager has crashed"
+	};
+	public static final int FLAG_GL_CLEAR = 0;
+
+	static {
+		try (RandomAccessFile raFile = new RandomAccessFile(new File("crash_states.bin"), "rw")) {
+			raFile.setLength(MAX_FUNCS);
+			crashBuffer = raFile.getChannel().map(FileChannel.MapMode.READ_WRITE, 0, MAX_FUNCS);
+			File output = new File("bad_logger.log");
+			boolean crashFound = false;
+			for (int i = 0; i < MAX_FUNCS; i++) {
+				if (crashBuffer.get(i) == (byte) 1) {
+					if (crashFound) {
+						if (output.exists())
+							output.delete();
+					}
+					crashFound = true;
+					appendToLog(output, MESSAGES[i]);
+				}
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void appendToLog(File output, String message) {
+		try (FileWriter writer = new FileWriter(output)) {
+			writer.append(message);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void enter(int index) {
+		if (crashBuffer != null)
+			crashBuffer.put(index, (byte) 1);
+	}
+
+	public static void exit(int index) {
+		if (crashBuffer != null)
+			crashBuffer.put(index, (byte) 0);
+	}
+}
--- src/main/java/com/sabrepenguin/crashlogger/mixin/MixinGLManager.java
+++ src/main/java/com/sabrepenguin/crashlogger/mixin/MixinGLManager.java
@@ -1,51 +1,22 @@
 package com.sabrepenguin.crashlogger.mixin;
 
+import com.sabrepenguin.crashlogger.helper.LogFileManager;
 import net.minecraft.client.renderer.GlStateManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mixin(value = GlStateManager.class)
 public class MixinGLManager {
-	@Unique
-	private static final Logger badLogger$LOGGER = LogManager.getLogger("CrashLogManager");
-	@Unique
-	private static final MappedByteBuffer badLogger$crashFlag;
-
-	static {
-		try (RandomAccessFile raFile = new RandomAccessFile(new File("gl_clear.bin"), "rw")) {
-			raFile.setLength(1);
-			badLogger$crashFlag = raFile.getChannel().map(FileChannel.MapMode.READ_WRITE, 0, 1);
-			badLogger$LOGGER.info("Crash flag initialized. Checking previous state");
-			if (badLogger$crashFlag.get(0) == (byte) 1) {
-				badLogger$LOGGER.error("Previous session crashed inside GLStateManager");
-				badLogger$crashFlag.put(0, (byte) 0);
-				throw new RuntimeException("Please report your crash to Sabre, resetting");
-			} else {
-				badLogger$LOGGER.info("Previous session crash was not inside the targeted function");
-			}
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
 	@Inject(at = @At("HEAD"), method = "clear(I)V")
 	private static void clearHead(int mask, CallbackInfo ci) {
-		badLogger$crashFlag.put(0, (byte) 1);
+		LogFileManager.enter(LogFileManager.FLAG_GL_CLEAR);
 	}
 
 	@Inject(at = @At("RETURN"), method = "clear(I)V")
 	private static void clearEnd(int mask, CallbackInfo ci) {
-		badLogger$crashFlag.put(0, (byte) 0);
+		LogFileManager.exit(LogFileManager.FLAG_GL_CLEAR);
 	}
 }
